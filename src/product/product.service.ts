9ed87613032ff37e6bca5471a6fe965d6ccc2640
cezarszl <cezarszlfedora@gmail.com>
2024-08-15 03:20:06 +0200

    feat: add findAll, findById, create, update and delete methods to ProductService

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { Product } from '@prisma/client';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    constructor(private readonly prisma: PrismaService) { }
+
+    async findAll(): Promise<Product[]> {
+        return this.prisma.product.findMany();
+    }
+
+    async findOne(id: number): Promise<Product | null> {
+        return this.prisma.product.findUnique({
+            where: { id },
+        });
+    }
+
+    async create(data: {
+        name: string;
+        description: string;
+        imageUrl: string;
+        price: number;
+        ownerId?: number; // Optional owner ID
+    }): Promise<Product> {
+        return this.prisma.product.create({
+            data,
+        });
+    }
+
+    async update(id: number, data: {
+        name?: string;
+        description?: string;
+        imageUrl?: string;
+        price?: number;
+    }): Promise<Product> {
+        return this.prisma.product.update({
+            where: { id },
+            data,
+        });
+    }
+
+    async delete(id: number): Promise<Product> {
+        return this.prisma.product.delete({
+            where: { id },
+        });
+    }
+}
