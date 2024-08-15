09eb931dedc886d2623c605638113d30fe494ad7
cezarszl <cezarszlfedora@gmail.com>
2024-08-15 03:12:31 +0200

    feat: add ProductController

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductService } from './product/product.service';
+import { ProductController } from './product/product.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ProductController],
   providers: [AppService, ProductService],
 })
 export class AppModule {}
