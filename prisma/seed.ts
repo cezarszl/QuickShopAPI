270a12036a46d5c880ca11cd3632b17356a59f19
cezarszl <cezarszlfedora@gmail.com>
2024-08-19 01:53:40 +0200

    chore: Add seeding to DB for tests

---
+++ prisma/seed.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    for (let i = 0; i < 10; i++) {
+        await prisma.user.create({
+            data: {
+                email: faker.internet.email(),
+                password: faker.internet.password(),
+                name: faker.person.fullName(),
+            },
+        });
+
+        await prisma.product.create({
+            data: {
+                name: faker.commerce.productName(),
+                description: faker.commerce.productDescription(),
+                imageUrl: faker.image.url(),
+                price: parseFloat(faker.commerce.price()),
+            },
+        });
+    }
+}
+
+main()
+    .catch(e => {
+        throw e;
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
