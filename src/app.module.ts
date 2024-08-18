85cc530761664f6c9b6a841b49482de459754512
cezarszl <cezarszlfedora@gmail.com>
2024-08-18 22:56:52 +0200

    test: Add unit tests for UserController

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { ProductService } from './product/product.service';
 import { ProductController } from './product/product.controller';
 import { PrismaService } from './prisma.service';
 import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductController],
+  controllers: [AppController, ProductController, UserController],
   providers: [AppService, ProductService, PrismaService, UserService],
 })
 export class AppModule { }
