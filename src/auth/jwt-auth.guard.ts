47c03bc0c1aab295a4b94c3e65bc11d1ef09861c
cezarszl <cezarszlfedora@gmail.com>
2024-08-19 16:53:39 +0200

    feat(auth): add JwtAuthGuard

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  canActivate(context: ExecutionContext): {
-    return super.canActivate(context);
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+    return super.canActivate(context) as boolean | Promise<boolean>;
   }
 }
