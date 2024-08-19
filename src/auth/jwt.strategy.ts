45bdf1e69b92e787f2982f91065a9b33c6c2d04c
cezarszl <cezarszlfedora@gmail.com>
2024-08-19 16:21:52 +0200

    feat(auth): add jwt-strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { UserService } from '../user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(private readonly userService: UserService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: process.env.JWT_SECRET || 'secretKey',
+        });
+    }
+
+    async validate(payload: any) {
+        return this.userService.findUserById(payload.sub);
+    }
+}
