ef20a1cc1092b6d9dc3ce46b48b50eca3c4f0a0d
cezarszl <cezarszlfedora@gmail.com>
2024-08-19 17:44:33 +0200

    fix: add decorators to Login and Register DTO

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,20 @@
-import { IsEmail, IsString, MinLength } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
+    @ApiProperty({
+        description: 'Email of the user',
+        example: 'user@example.com',
+    })
     @IsEmail()
     email: string;
 
+    @ApiProperty({
+        description: 'Password of the user',
+        example: 'P@ssw0rd',
+        minLength: 6,
+    })
     @IsString()
+    // @MinLength(6) // Optional: Ensure that password has a minimum length
     password: string;
 }
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,12 +1,27 @@
-import { IsEmail, IsString, MinLength } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
+    @ApiProperty({
+        description: 'Email of the user',
+        example: 'user@example.com',
+    })
     @IsEmail()
     email: string;
 
+    @ApiProperty({
+        description: 'Password of the user',
+        example: 'P@ssw0rd',
+        minLength: 6,
+    })
     @IsString()
+    @MinLength(6)
     password: string;
 
+    @ApiProperty({
+        description: 'Name of the user',
+        example: 'John Doe',
+    })
     @IsString()
     name: string;
 }
