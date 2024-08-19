4286caa255aa46b8ecb9d0135991ce5b812e5e7c
cezarszl <cezarszlfedora@gmail.com>
2024-08-20 01:00:32 +0200

    feat: update jwt secret key

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -10,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: process.env.JWT_SECRET || 'secretKey',
+            secretOrKey: process.env.JWT_SECRET,
         });
     }
 
