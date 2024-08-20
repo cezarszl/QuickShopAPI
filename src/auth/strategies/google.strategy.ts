0e22e519696796d97d27ecbe2d3e90eb9d259e06
cezarszl <cezarszlfedora@gmail.com>
2024-08-21 00:30:17 +0200

    feat(auth): add google strategy

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -3,7 +3,7 @@ import { Strategy, VerifyCallback } from "passport-google-oauth20";
 import { Injectable } from "@nestjs/common";
 import { AuthService } from "../auth.service";
 
-@Injectable
+@Injectable()
 
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     constructor(private authService: AuthService) {
@@ -11,7 +11,18 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: 'http://localhost:3000/auth/google/callback',
-
-        })
+            scope: ['email', 'profile'],
+        });
     }
+
+    async validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
+        const { emails } = profile;
+        const email = emails[0].value;
+        const googleId = profile.id;
+
+        const user = await this.authService.validateGoogleUser(googleID, email);
+
+        done(null, user)
+    }
+
 }
