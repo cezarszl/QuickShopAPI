5b33df1e57efab4cf92d12d441f7eeeeda651718
cezarszl <cezarszlfedora@gmail.com>
2024-09-23 06:04:56 +0200

    feat(category): add crud methods to service

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+    constructor(private readonly prisma: PrismaService) { }
+
+    async getAllCategories() {
+        return this.prisma.category.findMany();
+    }
+
+    async createCategory(data: Prisma.CategoryCreateInput) {
+        return this.prisma.category.create({
+            data
+        });
+    }
+
+    async updateCategory(id: number, data: Prisma.CategoryUpdateInput) {
+        return this.prisma.category.update({
+            where: { id },
+            data,
+        });
+    }
+
+    async deleteCategory(id: number) {
+        return this.prisma.category.delete(
+            { where: { id } });
+    }
+}
+
