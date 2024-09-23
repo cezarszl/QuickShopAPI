c65243fdf55f62569a8ff03e10f384c16550f45a
cezarszl <cezarszlfedora@gmail.com>
2024-09-23 20:12:40 +0200

    chore: seeded example catgories to db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,8 +4,26 @@ import { faker } from '@faker-js/faker';
 const prisma = new PrismaClient();
 
 async function main() {
+    // Utworzenie przykładowych kategorii
+    const categories = [
+        'Electronics',
+        'Books',
+        'Clothing',
+        'Home & Kitchen',
+        'Sports',
+    ];
+
+    for (const categoryName of categories) {
+        await prisma.category.create({
+            data: {
+                name: categoryName,
+            },
+        });
+    }
+
+
     for (let i = 0; i < 20; i++) {
-        await prisma.user.create({
+        const user = await prisma.user.create({
             data: {
                 email: faker.internet.email(),
                 password: faker.internet.password(),
@@ -13,9 +31,17 @@ async function main() {
             },
         });
 
+
+        const randomCategoryIndex = Math.floor(Math.random() * categories.length);
+        const category = await prisma.category.findUnique({
+            where: { name: categories[randomCategoryIndex] },
+        });
+
         await prisma.product.create({
             data: {
-                category: faker.commerce.productMaterial(),
+                category: {
+                    connect: { id: category.id },
+                },
                 name: faker.commerce.productName(),
                 description: faker.commerce.productDescription(),
                 imageUrl: faker.image.url(),
