7e528f2aac6da817c7938221fdacc33825f401c0
cezarszl <cezarszlfedora@gmail.com>
2024-11-08 22:47:03 +0100

    feat(colors): add get all, get by id, add and remove methods

--- src/colors/color.controller.ts
+++ src/colors/color.controller.ts
@@ -1,11 +1,60 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Delete, Get, HttpCode, Param, ParseIntPipe, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ColorService } from './color.service';
+import { Color } from '@prisma/client';
+import { ColorDto } from './dto/color.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CreateColorDto } from './dto/create.color.dto';
 
 
 @ApiTags('colors')
 @Controller('colors')
 export class ColorController {
-    constructor(private readonly colorService: Colr)
+    constructor(private readonly colorService: ColorService,
 
+    ) { }
+
+    // Get all colors
+    @Get()
+    @ApiOperation({ summary: 'Retrieve all colors' })
+    @ApiResponse({ status: 200, description: 'List of all colors', type: [ColorDto] })
+    async getAllColors(): Promise<Color[]> {
+        return this.colorService.getAllColors();
+    }
+
+    // Get a color by id
+    @Get(':id')
+    @ApiOperation({ summary: 'Retrieve a color by ID' })
+    @ApiParam({ name: 'id', description: 'Unique identifier of the color', type: Number })
+    @ApiResponse({ status: 200, description: 'Color name', type: ColorDto })
+    @ApiResponse({ status: 404, description: 'Color not found' })
+    async getColorById(@Param('id', ParseIntPipe) id: number): Promise<Color> {
+        return await this.colorService.getColorById(id);
+    }
+
+    // Add a new color
+    @Post()
+    @ApiBearerAuth()
+    @UseGuards(JwtAuthGuard)
+    @ApiOperation({ summary: 'Create a new color' })
+    @ApiBody({ type: CreateColorDto })
+    @ApiResponse({ status: 201, description: 'The color has been successfully created.', type: ColorDto })
+    @ApiResponse({ status: 400, description: 'Invalid input data.' })
+    async create(@Body() createColorDto: CreateColorDto): Promise<Color> {
+        return this.colorService.addColor(createColorDto);
+    }
+
+    // Delete a color
+    @Delete(':id')
+    @ApiBearerAuth()
+    @UseGuards(JwtAuthGuard)
+    @ApiOperation({ summary: 'Delete a color by ID' })
+    @ApiParam({ name: 'id', description: 'Unique identifier of the color to delete', type: Number })
+    @ApiResponse({ status: 204, description: 'The color has been successfully deleted.' })
+    @ApiResponse({ status: 404, description: 'Color not found' })
+    @HttpCode(204)
+    async delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
+        await this.colorService.delete(id);
+    }
 
 }
--- src/colors/color.service.ts
+++ src/colors/color.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { Color } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import { CreateColorDto } from './dto/create.color.dto';
 
 @Injectable()
-export class ColorService { }
+export class ColorService {
+    constructor(private readonly prisma: PrismaService) { }
+
+    async getAllColors(): Promise<Color[]> {
+        return this.prisma.color.findMany();
+    }
+
+    async getColorById(id: number): Promise<Color> {
+        const color = this.prisma.color.findUnique({
+            where: { id },
+        });
+        if (!color) {
+            throw new NotFoundException(`Color with ID ${id} not found`);
+        }
+        return color;
+    }
+
+    async addColor(createColorDto: CreateColorDto): Promise<Color> {
+
+        const existingColor = await this.prisma.color.findUnique({
+            where: { name: createColorDto.name },
+        });
+
+        if (existingColor) {
+            throw new ConflictException('Color with this name already exists');
+        }
+        return this.prisma.color.create({
+            data: createColorDto,
+        });
+    }
+
+    async delete(id: number): Promise<void> {
+        try {
+            await this.prisma.color.delete({
+                where: { id },
+            });
+        } catch (error) {
+            throw new NotFoundException(`Color with ID ${id} not found`);
+        }
+    }
+}
