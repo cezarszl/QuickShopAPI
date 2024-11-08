018efb919e844e1df08d893730163b55bf69de89
cezarszl <cezarszlfedora@gmail.com>
2024-11-09 00:54:36 +0100

    feat(brands): add get all, get by id, add and remove methods

--- src/brands/brand.controller.ts
+++ src/brands/brand.controller.ts
@@ -1,4 +1,60 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, ParseIntPipe, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { BrandService } from './brand.service';
+import { Brand } from '@prisma/client';
+import { BrandDto } from './dto/brand.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CreateBrandDto } from './dto/create.brand.dto';
 
+
+@ApiTags('brands')
 @Controller('brands')
-export class BrandController { }
+export class BrandController {
+    constructor(private readonly brandService: BrandService,
+
+    ) { }
+
+    // Get all brands
+    @Get()
+    @ApiOperation({ summary: 'Retrieve all brands' })
+    @ApiResponse({ status: 200, description: 'List of all brands', type: [BrandDto] })
+    async getAllBrands(): Promise<Brand[]> {
+        return this.brandService.getAllBrands();
+    }
+
+    // Get a brand by id
+    @Get(':id')
+    @ApiOperation({ summary: 'Retrieve a brand by ID' })
+    @ApiParam({ name: 'id', description: 'Unique identifier of the brand', type: Number })
+    @ApiResponse({ status: 200, description: 'Brand name', type: BrandDto })
+    @ApiResponse({ status: 404, description: 'Brand not found' })
+    async getBrandById(@Param('id', ParseIntPipe) id: number): Promise<Brand> {
+        return await this.brandService.getBrandById(id);
+    }
+
+    // Add a new brand
+    @Post()
+    @ApiBearerAuth()
+    @UseGuards(JwtAuthGuard)
+    @ApiOperation({ summary: 'Create a new brand' })
+    @ApiBody({ type: CreateBrandDto })
+    @ApiResponse({ status: 201, description: 'The brand has been successfully created.', type: BrandDto })
+    @ApiResponse({ status: 400, description: 'Invalid input data.' })
+    async create(@Body() createBrandDto: CreateBrandDto): Promise<Brand> {
+        return this.brandService.addBrand(createBrandDto);
+    }
+
+    // Delete a brand
+    @Delete(':id')
+    @ApiBearerAuth()
+    @UseGuards(JwtAuthGuard)
+    @ApiOperation({ summary: 'Delete a brand by ID' })
+    @ApiParam({ name: 'id', description: 'Unique identifier of the brand to delete', type: Number })
+    @ApiResponse({ status: 204, description: 'The brand has been successfully deleted.' })
+    @ApiResponse({ status: 404, description: 'Brand not found' })
+    @HttpCode(204)
+    async delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
+        await this.brandService.delete(id);
+    }
+
+}
--- src/brands/brand.service.ts
+++ src/brands/brand.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { Brand } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import { CreateBrandDto } from './dto/create.brand.dto';
 
 @Injectable()
-export class BrandService {}
+export class BrandService {
+    constructor(private readonly prisma: PrismaService) { }
+
+    async getAllBrands(): Promise<Brand[]> {
+        return this.prisma.brand.findMany();
+    }
+
+    async getBrandById(id: number): Promise<Brand> {
+        const brand = this.prisma.brand.findUnique({
+            where: { id },
+        });
+        if (!brand) {
+            throw new NotFoundException(`Brand with ID ${id} not found`);
+        }
+        return brand;
+    }
+
+    async addBrand(createBrandDto: CreateBrandDto): Promise<Brand> {
+
+        const existingBrand = await this.prisma.brand.findUnique({
+            where: { name: createBrandDto.name },
+        });
+
+        if (existingBrand) {
+            throw new ConflictException('Brand with this name already exists');
+        }
+        return this.prisma.brand.create({
+            data: createBrandDto,
+        });
+    }
+
+    async delete(id: number): Promise<void> {
+        try {
+            await this.prisma.brand.delete({
+                where: { id },
+            });
+        } catch (error) {
+            throw new NotFoundException(`Brand with ID ${id} not found`);
+        }
+    }
+}
