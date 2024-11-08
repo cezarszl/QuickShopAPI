de626af05a68635586445a54e1cb3c043d94c2e3
cezarszl <cezarszlfedora@gmail.com>
2024-11-08 22:44:36 +0100

    feat(colors): create  DTO for adding a new color

--- src/colors/dto/create.color.dto.ts
+++ src/colors/dto/create.color.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
-export class ColorDto {
+export class CreateColorDto {
     @ApiProperty({ example: 'New Color', description: 'Color of the product' })
     @IsString()
     name: string;
