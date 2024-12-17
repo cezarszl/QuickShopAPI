0d59113771cafb1952fda394aba6e6e8fb6ddd45
cezarszl <cezarszlfedora@gmail.com>
2024-12-17 16:38:43 +0100

    feat(category): add IsUnique validator

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
+import { IsUnique } from 'src/decorators/is-unique.decorator';
 
 export class CreateCategoryDto {
     @ApiProperty({ example: 'Electronics', description: 'Name of the category' })
+    @IsUnique('category', 'name')
     @IsString({ message: 'name must be a string' })
     name: string;
 }
