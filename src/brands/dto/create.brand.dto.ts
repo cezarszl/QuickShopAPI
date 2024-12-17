56e16b0dcad9a2160312ea0b6b4037b2b703dc99
cezarszl <cezarszlfedora@gmail.com>
2024-12-17 16:25:52 +0100

    feat(brand): add IsUnique validator

--- src/brands/dto/create.brand.dto.ts
+++ src/brands/dto/create.brand.dto.ts
@@ -1,8 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
-
+import { IsUnique } from 'src/decorators/is-unique.decorator';
 export class CreateBrandDto {
     @ApiProperty({ example: 'New Brand', description: 'Brand of the product' })
+    @IsUnique('brand', 'name')
     @IsString()
     name: string;
 }
