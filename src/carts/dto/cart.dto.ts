1bfc0b5a3abec30488129db7a70a09084aea1a9e
cezarszl <cezarszlfedora@gmail.com>
2025-01-07 23:17:29 +0100

    refactor: rename cartId to id in CartDto and update userId example

--- src/carts/dto/cart.dto.ts
+++ src/carts/dto/cart.dto.ts
@@ -8,21 +8,16 @@ export class CartDto {
         example: 'b0c57edb-93d7-49ad-81ea-7a8f079f3c7b',
     })
     @IsUUID('4', { message: 'Cart ID must be a valid UUID' })
-    cartId: string;
+    id: string;
 
     @ApiProperty({
         description: 'ID of the user who owns the cart (null for anonymous users)',
         required: false,
+        example: 126
     })
     @IsOptional()
     @IsInt({ message: 'User ID must be a valid integer' })
     userId?: number;
-
-    @ApiProperty({
-        description: 'List of items in the cart',
-        type: [CartItemDto],  // Używamy DTO dla elementów koszyka
-    })
-    items: CartItemDto[];
 
     @ApiProperty({
         description: 'Creation timestamp of the cart',
