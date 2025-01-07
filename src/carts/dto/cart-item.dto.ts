53aaca3ab2c94bef0b142bcb68edc7ad9fbde1fd
cezarszl <cezarszlfedora@gmail.com>
2025-01-07 23:28:16 +0100

    feat: add example values for cartId, productId, and quantity in CartItemDto documentation

--- src/carts/dto/cart-item.dto.ts
+++ src/carts/dto/cart-item.dto.ts
@@ -2,16 +2,21 @@ import { ApiProperty, ApiExtraModels } from '@nestjs/swagger';
 
 export class CartItemDto {
     @ApiProperty({
-        description: 'ID of the cart to which the item belongs. Forms composite primary key with productId.'
+        description: 'ID of the cart to which the item belongs. Forms composite primary key with productId.',
+        example: 'b0c57edb-93d7-49ad-81ea-7a8f079f3c7b'
     })
     cartId: string;
 
     @ApiProperty({
-        description: 'ID of the product in the cart. Forms composite primary key with cartId.'
+        description: 'ID of the product in the cart. Forms composite primary key with cartId.',
+        example: '56'
     })
     productId: number;
 
-    @ApiProperty({ description: 'Quantity of the product in the cart' })
+    @ApiProperty({
+        description: 'Quantity of the product in the cart',
+        example: '5'
+    })
     quantity: number;
 
     @ApiProperty({ description: 'Creation timestamp' })
