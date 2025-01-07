2c17c903d6123df998c01ae2c57ec905ea299296
cezarszl <cezarszlfedora@gmail.com>
2025-01-07 23:17:56 +0100

    feat: implement addCart method in CartService for creating a new cart

--- src/carts/cart.service.ts
+++ src/carts/cart.service.ts
@@ -1,12 +1,28 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
-import { CartItem } from '@prisma/client';
+import { Cart, CartItem, Prisma } from '@prisma/client';
 import { CreateCartItemDto } from './dto/create.cart-item.dto';
+import { CreateCartDto } from './dto/create.cart.dto';
 
 @Injectable()
 export class CartService {
     constructor(private readonly prisma: PrismaService) { }
 
+
+    async addCart(createCartDto: CreateCartDto): Promise<Cart> {
+
+        const { userId } = createCartDto;
+
+        const cart = await this.prisma.cart.create({
+            data: {
+                userId,
+            }
+        });
+
+        return cart;
+
+    }
+    
     async addItemToCart(createCartItemDto: CreateCartItemDto): Promise<CartItem> {
         const { cartId, productId, quantity } = createCartItemDto;
 
@@ -59,42 +75,5 @@ export class CartService {
 
         return newCartItem;
     }
-
-    // async removeItem(id: number): Promise<void> {
-    //     try {
-    //         await this.prisma.cartItem.delete({
-    //             where: { id },
-    //         });
-    //     } catch (error) {
-    //         throw new NotFoundException(`CartItem with ID ${id} not found`);
-    //     }
-    // }
-
-    // async clearCart(userId: number): Promise<void> {
-    //     const user = await this.prisma.user.findUnique({ where: { id: userId } });
-
-    //     if (!user) {
-    //         throw new NotFoundException(`User with ID ${userId} not found`);
-    //     }
-
-    //     await this.prisma.cartItem.deleteMany({
-    //         where: { userId: userId },
-    //     })
-    // }
-    // async updateQuantity(id: number, quantity: number): Promise<CartItem> {
-    //     try {
-    //         return await this.prisma.cartItem.update({
-    //             where: { id },
-    //             data: { quantity },
-    //         });
-    //     } catch (error) {
-    //         throw new NotFoundException(`CartItem with ID ${id} not found`);
-    //     }
-    // }
-
-    // async getCartItemsByUserId(userId: number): Promise<CartItem[]> {
-    //     return await this.prisma.cartItem.findMany({
-    //         where: { userId },
-    //     });
-    // }
+    
 }
