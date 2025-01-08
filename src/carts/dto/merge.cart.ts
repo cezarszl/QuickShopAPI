73f9f4e1911f753785d9c100392f07fc908b54f2
cezarszl <cezarszlfedora@gmail.com>
2025-01-08 08:13:50 +0100

    feat: add validation for userId and anonymousCartId in MergeCartDto

--- src/carts/dto/merge.cart.ts
+++ src/carts/dto/merge.cart.ts
@@ -1,12 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsUUID } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsUUID } from 'class-validator';
 
 export class MergeCartDto {
     @ApiProperty({ description: 'ID of the logged-in user', example: 1 })
     @IsNumber()
+    @IsNotEmpty()
     userId: number;
 
     @ApiProperty({ description: 'ID of the anonymous cart', example: '4aa036f0-ef0d-4498-831d-0888602c35b3' })
     @IsUUID('4', { message: 'Cart ID must be a valid UUID' })
+    @IsNotEmpty()
     anonymousCartId: string;
 }
